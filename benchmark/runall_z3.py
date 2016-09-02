d433b252995562b95a0777951f5b0738c68a78ae
Rahul Jain <rahuljain@Gugan.(none)>
2016-09-02 10:15:01 +0530

    script changes

--- benchmark/runall_z3.py
+++ benchmark/runall_z3.py
@@ -7,4 +7,4 @@ file_names = glob.glob(pwd + "/*.smt2")
 
 for i, val in enumerate(file_names):
 	print file_names[i]
-	subprocess.call(["../z3/buildd/z3", file_names[i]])
+	subprocess.call(['../z3/buildd/z3', '-T:30', file_names[i]])
--- benchmark/script.py
+++ benchmark/script.py
@@ -7,7 +7,7 @@ smt_file_name = sys.argv[1]  #store the current smt2 filename
 
 copyfile(smt_file_name, "gen_" + smt_file_name)  #create a copy of the smt2 file
 
-subprocess.call(["../z3/buildd/z3", smt_file_name]) # call modified z3, output in ".z3-trace" file
+subprocess.call(['../z3/buildd/z3', '-T:30', smt_file_name]) # call modified z3, output in ".z3-trace" file
 
 # delete line containing check-sat from file
 
