c32527310228ad2a504ff34963d15cb5161efef5
Rahul Jain <rahuljain@Gugan.(none)>
2016-09-02 10:02:39 +0530

    runall_z3.py

--- benchmark/runall_z3.py
+++ benchmark/runall_z3.py
@@ -1,6 +1,9 @@
 import subprocess 
 import glob
-file_names = glob.glob("/Users/rj/Bit-vector-multiplication-pattern/z3-master/benchmark/*.smt2")
+import os
+pwd = os.getcwd()
+
+file_names = glob.glob(pwd + "/*.smt2")
 
 for i, val in enumerate(file_names):
 	print file_names[i]
